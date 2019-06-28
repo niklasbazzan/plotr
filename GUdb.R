119a6f90a955d58ac9c1a9f0a209642e46d779f2
Niklas Bazzan <34390478+niklasbazzan@users.noreply.github.com>
2019-06-28 16:23:31 +0200

    - GUdb.R

--- GUdb.R
+++
@@ -1,9 +0,0 @@
-library(RMariaDB)
-
-localuserpassword <- "tSG7RWiMj8uK9vD97X2wUZf2kw0gOvV4"
-GUdb <- dbConnect(RMariaDB::MariaDB(), user='surveyuser', password=localuserpassword, dbname='surveydatabase', host='api.gieffektivt.no')
-dbListTables(GUdb)
-
-
-
-dbDisconnect(GUdb)
